7ec258c9ff6acdad91ec63a101866830794b76ca
Lucas Coelho Reichert <50890378+lucas3d4783@users.noreply.github.com>
2022-04-29 07:34:37 -0300

    Estruturação inicial e criação de rota principal

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Trending from '../views/Trending'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/trending',
+    name: 'trending',
+    component: Trending
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/',
+    name: 'home',
+    redirect: 'trending'
+  },
 ]
 
 const router = createRouter({
